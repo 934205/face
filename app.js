eda8d9a3beb9f02043ae697cb2bfd098a350ed74
934205 <nadesanjayarani4@gmail.com>
2025-09-26 21:05:42 +0530

    Updated backend: fixed Render port issue

--- app.js
+++ app.js
@@ -87,4 +87,5 @@ app.post("/verify", async (req, res) => {
   }
 });
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
